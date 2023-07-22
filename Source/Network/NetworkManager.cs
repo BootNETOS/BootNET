4b25d0ef995538e1fc9227d13394fe9dbb89252c
Pto Buon <ptobuon@outlook.it>
2023-07-22 13:04:09 +0200

    Add network

---
+++ Source/Network/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using Cosmos.System.Network.IPv4;
+using Cosmos.System.Network.IPv4.UDP.DHCP;
+using Cosmos.System.Network.IPv4.UDP.DNS;
+
+namespace BootNET.Network
+{
+    public static class NetworkManager
+    {
+        #region Methods
+        public static void Initialize(Address DNS)
+        {
+            using (var xClient = new DHCPClient())
+            {
+                xClient.SendDiscoverPacket();
+            }
+            DNSClient.Connect(DNS);
+        }
+        #endregion
+        #region Fields
+        public static DnsClient DNSClient;
+        #endregion
+    }
+}
