ad8053e450b76263cc6cca2eac06cc2428569ea7
Pto Buon <ptobuon@outlook.it>
2023-07-28 16:41:12 +0200

    Add keyboard layout and config

--- Source/Core/Installer.cs
+++ Source/Core/Installer.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BootNET.Filesystem;
+using Cosmos.System.ScanMaps;
+using System;
 
 namespace BootNET.Core
 {
     public class Installer
     {
+        public static string kbLayout;
         public static void InitialScreen()
         {
             Console.BackgroundColor = ConsoleColor.Blue;
@@ -65,7 +64,71 @@ namespace BootNET.Core
         }
         public static void DiskFormatting()
         {
-
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" " + Info.OS_Name + " " + Info.OS_Version + "Setup");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+            Console.WriteLine("  Formatting disk 0:\\...");
+            FilesystemManager.Format();
+            Console.WriteLine("  Done!");
+            Console.WriteLine("  Press any key to continue...");
+            KeyboardLayout();
+        }
+        public static void KeyboardLayout()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" " + Info.OS_Name + " " + Info.OS_Version + "Setup");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+            Console.WriteLine("  What is your preferred keyboard layout?");
+            Console.WriteLine("  You can set the keyboard layout by typing the number.");
+            Console.WriteLine("  If your preferred is not present, select US Standard..");
+            Console.WriteLine();
+            Console.WriteLine("  1. US Standard");
+            Console.WriteLine("  2. US Dvorak");
+            Console.WriteLine("  3. FR Standard");
+            Console.WriteLine("  4. DE Standard");
+            Console.WriteLine("  5. ES Standard");
+            Console.WriteLine("  6. GB Standard");
+            Console.WriteLine("  7. TR Standard");
+            Console.WriteLine();
+            Console.Write("  Your choice: ");
+            string input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new USStandardLayout());
+                    kbLayout = "USStandard";
+                    break;
+                case "2":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new US_Dvorak());
+                    kbLayout = "USDvorak";
+                    break;
+                case "3":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new FRStandardLayout());
+                    kbLayout = "FRStandard";
+                    break;
+                case "4":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new DEStandardLayout());
+                    kbLayout = "DEStandard";
+                    break;
+                case "5":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new ESStandardLayout());
+                    kbLayout = "ESStandard";
+                    break;
+                case "6":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new GBStandardLayout());
+                    kbLayout = "GBStandard";
+                    break;
+                case "7":
+                    Cosmos.System.KeyboardManager.SetKeyLayout(new TRStandardLayout());
+                    kbLayout = "TRStandard";
+                    break;
+            }
         }
     }
 }
