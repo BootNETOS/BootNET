cb88dbc13d4f8e682385a0969fc18764e777efc0
Pto Buon <ptobuon@outlook.it>
2023-07-27 20:02:09 -0700

    Add keyboard selection

---
+++ Source/Installer/Installer.cs
@@ -0,0 +1,72 @@
+﻿using Cosmos.System;
+using Cosmos.System.ScanMaps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootNET.Installer
+{
+    public static class Installer
+    {
+        static ConsoleKeyInfo info;
+        static string keyboard;
+        public static void InitScreen()
+        {
+            TUI.PrintInitialScreen();
+            KeyEvent keyEvent;
+            if (KeyboardManager.TryReadKey(out keyEvent))
+            {
+                switch (keyEvent.Key)
+                {
+                    case ConsoleKeyEx.Enter:
+                        KeyboardSelection();
+                        break;
+                    case ConsoleKeyEx.Q:
+                        Cosmos.System.Power.Reboot();
+                        break;
+                }
+            }
+        }
+        public static void KeyboardSelection()
+        {
+            TUI.PrintKeyboardScreen();
+            KeyEvent keyEvent;
+            if (KeyboardManager.TryReadKey(out keyEvent))
+            {
+                switch (keyEvent.Key)
+                {
+                    case ConsoleKeyEx.Num1:
+                        KeyboardManager.SetKeyLayout(new USStandardLayout());
+                        keyboard = "uss";
+                        break;
+                    case ConsoleKeyEx.Num2:
+                        KeyboardManager.SetKeyLayout(new DEStandardLayout());
+                        keyboard = "des";
+                        break;
+                    case ConsoleKeyEx.Num3:
+                        KeyboardManager.SetKeyLayout(new ESStandardLayout());
+                        keyboard = "ess";
+                        break;
+                    case ConsoleKeyEx.Num4:
+                        KeyboardManager.SetKeyLayout(new FRStandardLayout());
+                        keyboard = "frs";
+                        break;
+                    case ConsoleKeyEx.Num5:
+                        KeyboardManager.SetKeyLayout(new US_Dvorak());
+                        keyboard = "usd";
+                        break;
+                    case ConsoleKeyEx.Num6:
+                        KeyboardManager.SetKeyLayout(new GBStandardLayout());
+                        keyboard = "gbs";
+                        break;
+                    case ConsoleKeyEx.Num7:
+                        KeyboardManager.SetKeyLayout(new TRStandardLayout());
+                        keyboard = "trs";
+                        break;
+                }
+            }
+        }
+    }
+}
--- Source/Installer/TUI.cs
+++ Source/Installer/TUI.cs
@@ -9,9 +9,10 @@ namespace BootNET.Installer
 {
     public class TUI
     {
-        public static void PrintScreen()
+        public static void PrintInitialScreen()
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine();
             Console.WriteLine(" " + Info.OS_Name + " " + Info.OS_Version + " Setup");
@@ -22,8 +23,25 @@ namespace BootNET.Installer
             Console.WriteLine("This will install system files to the disk.");
             Console.WriteLine();
             Console.WriteLine("     - To setup a new installation, press ENTER");
-            Console.WriteLine("     - To repair a previous installation press R");
+            Console.WriteLine("     - To repair a previous installation press R (WIP)");
             Console.WriteLine("     - To quit and restart, press Q.");
+        }
+        public static void PrintKeyboardScreen()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" " + Info.OS_Name + " " + Info.OS_Version + " Setup");
+            Console.WriteLine("====================");
+            Console.WriteLine();
+            Console.WriteLine("Please your preferred keyboard map. To set it, press the number.");
+            Console.WriteLine();
+            Console.WriteLine("     1. US_Standard");
+            Console.WriteLine("     2. DE_Standard");
+            Console.WriteLine("     3. ES_Standard");
+            Console.WriteLine("     4. FR_Standard");
+            Console.WriteLine("     5. US_Dvorak");
+            Console.WriteLine("     6. GB_Standard");
+            Console.WriteLine("     7. TR_Standard");
         }
     }
 }
