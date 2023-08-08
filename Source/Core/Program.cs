cb51f9b7350ed8f8826ec8bde15a574a7779fe01
Pto Buon <ptobuon@outlook.it>
2023-08-08 15:26:11 +0200

    Cleanup

--- Source/Core/Program.cs
+++ Source/Core/Program.cs
@@ -5,11 +5,9 @@ namespace BootNET.Core
 {
     public class Program : Kernel
     {
-        public static CommandManager commandManager;
         protected override void BeforeRun()
         {
             BootManager.Boot();
-
         }
 
         protected override void Run()
