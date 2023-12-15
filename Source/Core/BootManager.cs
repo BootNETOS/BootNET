c7efe310749005b365f062f96ca997c646f75d52
Pto Buon <ptobuon@outlook.it>
2023-12-15 21:16:09 +0100

    Updates

--- Source/Core/BootManager.cs
+++ Source/Core/BootManager.cs
@@ -1,69 +1,68 @@
-﻿using PrismAPI.Graphics;
-using PrismAPI.Graphics.Animation;
-using PrismAPI.Hardware.GPU;
-using System.Threading;
+﻿using Cosmos.HAL.Drivers.Audio;
+using Cosmos.System.Audio;
+using Cosmos.System.FileSystem;
+using Cosmos.System.FileSystem.VFS;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using Cosmos.System.Network.IPv4.UDP.DHCP;
+using System;
 
 namespace BootNET.Core
 {
     public static class BootManager
     {
-        private static readonly AnimationController A;
-        private static readonly AnimationController B;
-        private static readonly ColorController C;
-        private static Display Canvas = null!;
-        private static bool IsEnabled;
-        private static bool IsSetup;
-        static BootManager()
+        public static void Boot()
         {
-            A = new(25f, 270f, new(0, 0, 0, 0, 750), AnimationMode.Ease);
-            B = new(0f, 360f, new(0, 0, 0, 0, 500), AnimationMode.Linear);
-            C = new(Color.Transparent, Color.Black, new(0, 0, 0, 0, 500), AnimationMode.Ease);
-            A.IsContinuous = true;
-        }
-        private static void Update()
-        {
-            if (B.IsFinished)
+            //Setting VGA Font
+            VGAScreen.SetFont(PCScreenFont.Default.CreateVGAFont(), PCScreenFont.Default.Height);
+            Console.Clear();
+            Console.WriteLine("Connecting using Ethernet...");
+            try
             {
-                B.Reset();
+                //DHCP Client
+                using (var xClient = new DHCPClient())
+                {
+                    xClient.SendDiscoverPacket();
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connected successfully.");
+                Console.ResetColor();
             }
-
-            ushort H3 = (ushort)(Canvas.Height / 3);
-            ushort W2 = (ushort)(Canvas.Width / 2);
-            ushort H2 = (ushort)(Canvas.Height / 2);
-
-            Canvas.Clear();
-            Canvas.DrawImage(W2 - (Resources.BootLogo.Width / 2), H2 - (Resources.BootLogo.Height / 2), Resources.BootLogo, false);
-
-            int LengthOffset = (int)(B.Current + A.Current);
-            int Offset = (int)B.Current;
-
-            Canvas.DrawArc(W2, H2 + (H2 / 2) + (H3 / 2), 19, Color.LightGray, Offset, LengthOffset);
-            Canvas.DrawArc(W2, H2 + (H2 / 2) + (H3 / 2), 20, Color.White, Offset, LengthOffset);
-            Canvas.DrawArc(W2, H2 + (H2 / 2) + (H3 / 2), 21, Color.LightGray, Offset, LengthOffset);
-            Canvas.Update();
-        }
-
-        public static void Show(Display Canvas)
-        {
-            if (IsSetup)
+            catch (Exception ex)
             {
-                IsEnabled = true;
-                return;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error while connecting: " + ex.Message);
+                Console.ResetColor();
             }
-
-            // Assign the canvas instance.
-            BootManager.Canvas = Canvas;
-
-            // Add a timer to update the screen while booting.
-            Timer T = new((_) => { if (IsEnabled) { Update(); } }, null, 55, 0);
-            IsEnabled = true;
-            IsSetup = true;
-        }
-
-        public static void Hide()
-        {
-            IsEnabled = false;
-            C.Reset();
+            try
+            {
+                CosmosVFS fs = new();
+                VFSManager.RegisterVFS(fs, false);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Filesystem enabled");
+                Console.ResetColor();
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Filesystem disabled: " + ex.Message);
+                Console.ResetColor();
+            }
+            try
+            {
+                AC97.Initialize(bufferSize: 4096);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Audio disabled: " + ex.Message);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to BootNET!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
         }
     }
 }
--- Source/Core/Program.cs
+++ Source/Core/Program.cs
@@ -1,31 +1,16 @@
-﻿using BootNET.Audio;
-using BootNET.GUI;
-using BootNET.Network;
-using Cosmos.System;
-using PrismAPI.Hardware.GPU;
+﻿using Cosmos.System;
 
 namespace BootNET.Core
 {
     public class Program : Kernel
     {
-        public static Display Canvas;
         protected override void BeforeRun()
         {
-            Canvas = Display.GetDisplay(1280, 720);
-            BootManager.Show(Canvas);
-            try
-            {
-                AudioManager.Initialize();
-                NetworkManager.Initialize();
-            }
-            catch { }
-            BootManager.Hide();
-            Canvas.Update();
-            WindowManager.Start();
+            BootManager.Boot();
         }
         protected override void Run()
         {
-            WindowManager.Update();
+            
         }
     }
 }
