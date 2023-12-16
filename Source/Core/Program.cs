5e1692e4f283e99d6eead3a56f806b13a5e84121
Pto Buon <ptobuon@outlook.it>
2023-12-16 17:54:05 +0100

    Fix

--- Source/Core/Program.cs
+++ Source/Core/Program.cs
@@ -7,18 +7,23 @@ namespace BootNET.Core
     public class Program : Kernel
     {
         public static string Username;
-        public static bool LoggedIn;
+        public static bool LoggedIn = false;
         public static CommandManager CommandManager = new();
         public static string CurrentDirectory = "0:\\";
         protected override void BeforeRun()
         {
             BootManager.Boot();
+            CommandManager.RegisterCommand();
         }
         protected override void Run()
         {
             if(LoggedIn)
             {
-                CommandManager.HandleConsole(Username, CurrentDirectory, System.ConsoleColor.Magenta, System.ConsoleColor.Blue, '@');
+                CommandManager.HandleConsole(Username, CurrentDirectory, System.ConsoleColor.Magenta, System.ConsoleColor.Blue, ':');
+            }
+            else
+            {
+                LoginSystem.Login();
             }
         }
     }
