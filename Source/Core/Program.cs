3de4f84143b45054f8e9d50a8f36079623fa2f8d
Pto Buon <ptobuon@outlook.it>
2024-06-10 08:43:17 +0200

    Update

--- Source/Core/Program.cs
+++ Source/Core/Program.cs
@@ -6,14 +6,18 @@ namespace BootNET.Core
 {
     public class Program : Kernel
     {
+        #region Properties
         public static string OS_Name = "BootNET";
         public static string OS_Version = "24.07";
         public static string CPU_Vendor = CPU.GetCPUVendorName();
         public static string CPU_Brand_String = CPU.GetCPUBrandString();
         public static string CPU_CycleSpeed = CPU.GetCPUCycleSpeed().ToString();
         public static string Total_RAM = CPU.GetAmountOfRAM().ToString();
-        public 
-        NtfsCosmosVFS FileSystem;
+        #endregion
+        #region Drivers
+        public static NtfsCosmosVFS FileSystem;
+        #endregion
+
         protected override void BeforeRun()
         {
         }
